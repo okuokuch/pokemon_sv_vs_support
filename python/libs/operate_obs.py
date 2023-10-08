e1b3d0ebc0922ca6cc28ddd13f44e65eb0ab4080
okuokuch <okuokuch@gmail.com>
2023-10-08 22:19:32 +0900

    PEP8に沿った修正。

--- python/libs/operate_obs.py
+++ python/libs/operate_obs.py
@@ -1,16 +1,20 @@
 import obsws_python as obs
 import obsws_python.error as obs_error
 
+
 class OBS:
     """OBSの操作をするクラス。"""
 
-    def __init__(self, port:int, password:str) -> None:
+    def __init__(self, port: int, password: str) -> None:
         """ポートとパスワードを用いてOBSに接続する。"""
         self.connect(port, password)
 
-    def connect(self, port:int, password:str)->None:
+    def connect(self, port: int, password: str) -> None:
         try:
-            self.ws = obs.ReqClient(host='localhost', port=port, password=password)
+            self.ws = obs.ReqClient(
+                host='localhost',
+                port=port,
+                password=password)
         except ConnectionRefusedError as e:
             print(e)
             print('接続できませんでした。')
@@ -18,7 +22,13 @@ class OBS:
             print(e)
             print('portかパスワードを確認してください。')
 
-    def take_screenshot(self, source_name:str, file_path:str, img_format:str = 'png', width:int = None, height:int = None)->None:
+    def take_screenshot(
+            self,
+            source_name: str,
+            file_path: str,
+            img_format: str = 'png',
+            width: int = None,
+            height: int = None) -> None:
         """指定したシーンのスクショを保存する。
 
         param:
@@ -28,7 +38,14 @@ class OBS:
             width:保存する画像サイズの幅。未記入の場合、OBSで設定している幅となる。
             height:保存する画像サイズの高さ。未記入の場合、OBSで設定している高さとなる。
         """
-        self.ws.save_source_screenshot(source_name, img_format, file_path, width, height, -1)
+        self.ws.save_source_screenshot(
+            source_name,
+            img_format,
+            file_path,
+            width,
+            height,
+            -1)
+
 
 if __name__ == '__main__':
     test = OBS(4455, 'obwgzSCmFWzpaJaq')
